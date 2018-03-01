c553c954dd70e0d9d0c0535e88b9d474e05c904c
MahendranR95 <mahendranr18795@gmail.com>
2018-03-01 20:57:20 +0530

    Create ReverseNo.java

---
+++ ReverseNo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ReverseNo {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("Input :");
+		String s=in.nextLine();
+		if(s.length()>0&&s.length()<=100000)
+		{
+			for(int i=s.length()-1;i>=0;i--)
+			{
+				System.out.print(s.charAt(i));
+			}
+		}
+	}
+}
