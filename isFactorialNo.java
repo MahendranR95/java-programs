7a10ee813e241c144579e558e919915fad09bde9
MahendranR95 <mahendranr18795@gmail.com>
2018-03-08 20:14:49 +0530

    Create isFactorialNo.java

---
+++ isFactorialNo.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class isFactorialNo {
+
+	public static void main(String[] args) {
+    	// TODO Auto-generated method stub
+	Scanner in=new Scanner(System.in);
+	System.out.println("Input:");
+	int n=in.nextInt();
+	int count=0;
+	for(int i=1;i<=n;i++)
+	{
+	  count=isFact(i);
+  } 
+	System.out.println(count);
+	
+}
+
+private static int isFact(int n) {
+	// TODO Auto-generated method stub
+	int c=1;
+	for(;n!=0;n--){
+		c=c*n;
+	}		
+	return c;
+}
+
+}
