c74d9d751fc51fc37f06ca10bf11a8ab887ccfa0
MahendranR95 <mahendranr18795@gmail.com>
2018-03-20 17:35:09 +0530

    Create PrimeNoBetweenRange1.java

---
+++ PrimeNoBetweenRange1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class PrimeNoBetweenRange {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("Input :");
+		int s=in.nextInt();
+		int e=in.nextInt();
+		int co=0;
+		for(int j=s+1;j<e;j++)
+		{
+			int c=0;
+			int i=1;
+			while(i<=j)
+			{
+				if(j%i==0)
+				{
+					c++;
+				}
+				i++;
+			}
+			if(c==2)
+			{
+				System.out.println(j);
+			}
+				
+		}
+		
+	}
+}
